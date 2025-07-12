821aff83002834da2ace0f7ce3a3d0f05ea6e391
avinys <arvydasvinys@gmail.com>
2025-07-12 20:04:29 +0200

    Added styling and separated some logic into reusable components

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Arvydas</div>;
+}
+
+export default Username;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,23 +5,24 @@ import CartOverview from "../features/cart/CartOverview";
 import Loader from "./Loader";
 
 function AppLayout() {
-    const navigation = useNavigation();
-    const isLoading = navigation.state === "loading";
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
-    return (
-        <div className="layout">
-            {isLoading && <Loader />}
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
 
-            <Header />
+      <Header />
 
-            <main>
-                <h1>Content</h1>
-                <Outlet />
-            </main>
+      <div className="overflow-auto">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
-            <CartOverview />
-        </div>
-    );
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
