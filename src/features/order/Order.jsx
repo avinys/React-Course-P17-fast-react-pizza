aafcfae54e1f704706f25027b8ef5c3641aae368
Arvydas Vingis <arvydasvinys@gmail.com>
2025-07-22 20:37:11 +0300

    Using useFetcher for ingredient fetcher in past order view

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,5 +1,5 @@
 // Test ID: IIDSAT
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 
 import { getOrder } from "../../services/apiRestaurant";
 import {
@@ -8,44 +8,16 @@ import {
   formatDate,
 } from "../../utilities/helpers";
 import OrderItem from "./OrderItem";
-
-// const order = {
-//     id: "ABCDEF",
-//     customer: "Jonas",
-//     phone: "123456789",
-//     address: "Arroios, Lisbon , Portugal",
-//     priority: true,
-//     estimatedDelivery: "2027-04-25T10:00:00",
-//     cart: [
-//         {
-//             pizzaId: 7,
-//             name: "Napoli",
-//             quantity: 3,
-//             unitPrice: 16,
-//             totalPrice: 48,
-//         },
-//         {
-//             pizzaId: 5,
-//             name: "Diavola",
-//             quantity: 2,
-//             unitPrice: 16,
-//             totalPrice: 32,
-//         },
-//         {
-//             pizzaId: 3,
-//             name: "Romana",
-//             quantity: 1,
-//             unitPrice: 15,
-//             totalPrice: 15,
-//         },
-//     ],
-//     position: "-9.000,38.000",
-//     orderPrice: 95,
-//     priorityPrice: 19,
-// };
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+  useEffect(() => {
+    if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+  }, [fetcher]);
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -88,7 +60,15 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+            isLoadingIngredients={fetcher.state === "loading"}
+          />
         ))}
       </ul>
 
